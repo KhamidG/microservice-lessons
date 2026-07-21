a0566f715641167fee103a95abe22382caa34c83
Khamid Gaipov <gaipovkhamid.uz@gmail.com>
2026-07-21 23:25:12 +0500

    Initial commit

---
+++ iam_serivce/src/main/java/com/post_hub/iam_serivce/IamSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.post_hub.iam_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamSerivceApplication.class, args);
+	}
+
+}
